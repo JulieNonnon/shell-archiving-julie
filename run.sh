ec96147fe7de2aa8b9b4950774e207f4a922e6e9
JulieNonnon <julie.nonnon@gmail.com>
2025-11-10 14:51:46 +0100

    updated run.sh with the absolute path

--- run.sh
+++ run.sh
@@ -4,7 +4,11 @@ echo "hello world :D !"
 # current date an hour :
 
 DATE=$( date '+%F_%H:%M:%S' )
-echo "$DATE"
+echo "Bash script starting at : $DATE"
 
 # file path :
+SCRIPT_PATH=$( realpath "$0" )
+echo "Script full path : $SCRIPT_PATH"
+
+# dowloading the json from each urls inside urls.txt
 
