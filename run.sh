b4ed94a55b8412dc78def86fa1286c0002b3e288
JulieNonnon <julie.nonnon@gmail.com>
2025-11-10 15:07:33 +0100

    updated run.sh with the creation of a temporary directory

--- run.sh
+++ run.sh
@@ -7,8 +7,16 @@ DATE=$( date '+%F_%H:%M:%S' )
 echo "Bash script starting at : $DATE"
 
 # file path :
+
 SCRIPT_PATH=$( realpath "$0" )
 echo "Script full path : $SCRIPT_PATH"
+
+# create a temporary directory to store temporary files :
+# note : if custom name, add at least 3 "X" at the end to ensure uniqueness
+
+TEMP_DIR=$( mktemp -d "temporary.XXXXXX" )
+
+echo "Temporary directory created as $TEMP_DIR"
 
 # dowloading the json from each urls inside urls.txt
 
